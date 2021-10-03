913e6ae5a235c628a065d18f7e5993f6f467cec4
Saulo Costa <saulomlcosta10@gmail.com>
2021-10-03 15:36:17 -0300

    Sidebar wich contains more infor about users.

---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import { Container, User, Avatar, Role } from "./styles";
+
+interface UserProps {
+  nickname: string;
+  isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({ nickname, isBot}) => {
+  return (
+    <User>
+      <Avatar className= {isBot ? 'bot' : ''}/>
+
+      <strong>{nickname}</strong>
+
+      {isBot && <span>Bot</span>}     
+    </User>
+  );
+};
+
+const UserList: React.FC = () => {
+  return (
+    <Container>
+      <Role>Available - 1</Role>
+      <UserRow nickname="NkSvTeN" />
+
+      <Role>Offline - 18</Role>
+      <UserRow nickname="Firu" isBot />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+      <UserRow nickname="OtherUsers" />     
+    </Container>
+  );
+};
+
+export default UserList;
